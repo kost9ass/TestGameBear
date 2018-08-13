f824f6d7c04fc07de83d202234f86f746505ce10
Kostya <kost9ass@gmail.com>
2018-08-13 11:40:34 +0300

    Initial commit

---
+++ TestWork/Assets/CustomizeB.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizeB : MonoBehaviour {
+
+    public Material[] Mat = new Material[8];
+    public GameObject Body;
+    public GameObject[] UFoot = new GameObject[2];
+    public GameObject[] DFoot = new GameObject[2];
+    public GameObject Galst;
+    Animator Anim;
+    Material matm;
+    int AnimDrag;
+    public int CustSet;
+    // Use this for initialization
+    void Start () {               
+        Anim = GetComponent<Animator>();
+        Galst.SetActive(false);
+        //rend = Bear.GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(CustSet == 1 || CustSet == 2)
+        {
+            AnimDrag = 1;
+            if (CustSet == 1)
+            {
+                Body.GetComponent<Renderer>().material = Mat[0];              
+            }
+            if (CustSet == 2)
+            {
+                Body.GetComponent<Renderer>().material = Mat[1];
+            }
+            CustSet = 0;
+        }
+        if (CustSet == 3 || CustSet == 4)
+        {
+            AnimDrag = 2;
+            if (CustSet == 3)
+            {
+                UFoot[0].GetComponent<Renderer>().material = Mat[2];
+                UFoot[1].GetComponent<Renderer>().material = Mat[2];
+                DFoot[0].GetComponent<Renderer>().material = Mat[4];
+                DFoot[1].GetComponent<Renderer>().material = Mat[4];
+            }
+            if (CustSet == 4)
+            {
+                UFoot[0].GetComponent<Renderer>().material = Mat[3];
+                UFoot[1].GetComponent<Renderer>().material = Mat[3];
+                DFoot[0].GetComponent<Renderer>().material = Mat[5];
+                DFoot[1].GetComponent<Renderer>().material = Mat[5];
+            }
+            CustSet = 0;
+        }
+        if (CustSet == 5 || CustSet == 6)
+        {
+            AnimDrag = 3;
+            if (CustSet == 5)
+            {
+                Galst.SetActive(true);
+                Galst.GetComponent<Renderer>().material = Mat[6];
+            }
+            if (CustSet == 6)
+            {
+                Galst.SetActive(true);
+                Galst.GetComponent<Renderer>().material = Mat[7];
+            }
+            CustSet = 0;
+        }
+        if (AnimDrag == 1)
+        {   
+            Anim.Play("BodyAn");
+            AnimDrag = 0;
+        }
+        if (AnimDrag == 2)
+        {
+            Anim.Play("FootAn");
+            AnimDrag = 0;
+        }
+        if (AnimDrag == 3)
+        {
+            Anim.Play("GalstAn");
+            AnimDrag = 0;
+        }
+
+    }
+}
---
+++ TestWork/Assets/Drag_and_Drop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag_and_Drop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    
+    public int Podset;
+    GameObject Bear;
+    Vector3 ItemPos;
+    Vector3 DropPos;
+    public CustomizeB Var;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Bear = GameObject.Find("BearMesh");
+        Var = Bear.GetComponent<CustomizeB>();
+        DropPos = GameObject.Find("DropArea").transform.position;
+        ItemPos = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (transform.position.x <= DropPos.x+90 && transform.position.x >= DropPos.x - 90 && transform.position.y<= DropPos.y+90 && transform.position.y >= DropPos.y - 90)
+        {           
+            transform.position = ItemPos;
+            Var.CustSet = Podset;
+        }
+        transform.position = ItemPos;
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
